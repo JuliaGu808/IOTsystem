cb10eb2f49a02d93a323934a35df8c9fbcb9eb36
Julia Gu <juliagu808@gmail.com>
2021-05-17 14:46:20 +0200

    init

---
+++ intelliji/src/main/java/com/example/intelliji/IntellijiApplication.java
@@ -0,0 +1,13 @@
+package com.example.intelliji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellijiApplication.class, args);
+    }
+
+}
---
+++ intelliji/src/test/java/com/example/intelliji/IntellijiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intelliji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
