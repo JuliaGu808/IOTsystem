7fd0af6ac28409664efe24e185fb16662f3d209a
JuliaGu808 <43201212+JuliaGu808@users.noreply.github.com>
2021-05-21 09:42:47 +0200

    Update device.js
    change header

--- intelliji/src/main/resources/static/device.js
+++ intelliji/src/main/resources/static/device.js
@@ -18,11 +18,11 @@ function refresh(){
                         ("0" + recordtime.getUTCHours()).slice(-2) + ":" +
                         ("0" + recordtime.getUTCMinutes()).slice(-2) + ":" +
                         ("0" + recordtime.getUTCSeconds()).slice(-2);
-                    $("#device_data").append("<tr><td>" + item.deviceName + "</td>" +
+                    $("#device_data").append("<tr><td>" + item.deviceHolder + "</td>" +
                         "<td>" + item.temperature + "</td>" +
                         "<td>" + item.humidity + "</td>" +
                         "<td>" + dateString + "</td>" +
-                        "<td>" + item.deviceHolder + "</td></tr>");
+                        "<td>" + item.deviceName + "</td></tr>");
                 })
             }
             else{
